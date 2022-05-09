f7d8abdf7cbca1fe0af9c687db1928295c074f48
Timothee Simon <tim@mirahi.io>
2022-05-09 21:06:43 +0200

    fix: 🐛 fixed entity_id type

--- src/index.ts
+++ src/index.ts
@@ -38,24 +38,24 @@ export let stateListener: <T>(callback: (event: stateChangeEvent<T>) => void) =>
   throw new Error('Connection was not initialized');
 };
 
-export const listenForEntity: <T>(entity: HassEntity, callback: (event: stateChangeEvent<T>) => void) => void = (entity, callback) =>{
+export const listenForEntity: <T>(entity_id: string , callback: (event: stateChangeEvent<T>) => void) => void = (entity_id, callback) =>{
   stateListener(event => {
-    if(event.data.entity_id === entity.entity_id)
+    if(event.data.entity_id === entity_id)
     { // @ts-ignore
       callback(event)
     }
   })
 }
 
-export const onEntitiesState: <T>(entitiesState: {entity: HassEntity, state: number|boolean|string}[], callback: (event: stateChangeEvent<T>) => void) => void = (entitiesState, callback) =>{
-  entitiesState.forEach(({entity, state}) => {
-    listenForEntity(entity, event => {
-      const isAllEntitiesCorrect = entitiesState.reduce((isCorrect, {entity, state})=>{
+export const onEntitiesState: <T>(entitiesState: {entity_id: string, state: number|boolean|string}[], callback: (event: stateChangeEvent<T>) => void) => void = (entitiesState, callback) =>{
+  entitiesState.forEach(({entity_id, state}) => {
+    listenForEntity(entity_id, event => {
+      const isAllEntitiesCorrect = entitiesState.reduce((isCorrect, {entity_id, state})=>{
         if(!isCorrect)
           return false
-        if(entity.entity_id === event.data.entity_id && state === event.data.new_state.state)
+        if(entity_id === event.data.entity_id && state === event.data.new_state.state)
           return true
-        return shadowState[entity.entity_id].state === state;
+        return shadowState[entity_id].state === state;
       }, true)
 
       if(isAllEntitiesCorrect)
@@ -66,8 +66,8 @@ export const onEntitiesState: <T>(entitiesState: {entity: HassEntity, state: num
   })
 }
 
-export const listenForEntites: <T>(entities: HassEntity[], callback: (event: stateChangeEvent<T>) => void) => void = (entities, callback) => {
-  entities.forEach(entity => listenForEntity(entity, callback))
+export const listenForEntites: <T>(entities_id: string[], callback: (event: stateChangeEvent<T>) => void) => void = (entities_id, callback) => {
+  entities_id.forEach(entity_id => listenForEntity(entity_id, callback))
 }
 
 export const configure = async ({
