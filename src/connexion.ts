00e2a3f2fb536183efeb4613adf7a7e5caedcab5
Timothee Simon <tim@mirahi.io>
2022-05-13 23:04:12 +0200

    fixed type

--- src/connexion.ts
+++ src/connexion.ts
@@ -17,7 +17,7 @@ const MSG_TYPE_AUTH_OK = 'auth_ok';
 const ERR_CANNOT_CONNECT = 1;
 const ERR_INVALID_AUTH = 2;
 
-export let shadowState = {} as { [x in string]: unknown  };
+export let shadowState = {} as { [x in string]: object  };
 
 export type stateChangeEvent<T = unknown> = {
   data: {
