30887ac0ce6ff0d9456df76ff4fb2a2dd3053d8c
Timothee Simon <tim@mirahi.io>
2022-05-14 14:42:43 +0200

    feat(deprecated function): remove deprecated functions

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,75 +1,8 @@
-import { shadowState, StateChangeEvent, stateListener } from './connexion';
-import { EntityId } from './ha-types/common';
+import { StateChangeEvent, stateListener } from './connexion';
+import { EntityId } from './ha-types';
 import {HassEntity, HassServiceTarget} from "home-assistant-js-websocket";
 import {isString} from "./utils/isString";
 import {isRegExp} from "util/types";
-
-/**
- * @deprecated Use effect instead
- */
-export const listenForEntity: <T>(
-  entity:
-    | EntityId
-    | EntityId[]
-    | RegExp
-    | ((event: StateChangeEvent<T>) => boolean),
-  callback: (event: StateChangeEvent<T>) => void,
-) => void = (entity, callback) => {
-  if (typeof entity === 'string')
-    stateListener((event) => {
-      if (event.data.entity_id === entity) callback(event);
-    });
-  else if ('exec' in entity)
-    stateListener((event) => {
-      if (event.data.entity_id.match(entity)) callback(event);
-    });
-  else if (typeof entity === 'function') {
-    stateListener((event) => {
-      if (entity(event)) {
-        callback(event);
-      }
-    });
-  } else
-    entity.forEach((id) =>
-      stateListener((event) => {
-        if (event.data.entity_id === id) callback(event);
-      }),
-    );
-};
-
-
-/**
- * @deprecated Use effect instead
- */
-export const onEntitiesStates: <T>(
-  entitiesState: ({ entity_id: EntityId } & Partial<{
-    state: number | boolean | string;
-    not: number | boolean | string;
-  }>)[],
-  callback: (event: StateChangeEvent<T>) => void,
-  otherwise?: (event: StateChangeEvent<T>) => void,
-) => void = (entitiesState, callback, otherwise) => {
-  entitiesState.forEach(({ entity_id, state: currentEntityState }) => {
-    listenForEntity(entity_id, (event) => {
-      const isAllEntitiesCorrect = entitiesState.reduce(
-        (isCorrect, { entity_id, state }) => {
-          if (!isCorrect) return false;
-          if (
-            entity_id === event.data.entity_id &&
-            currentEntityState === event.data.new_state.state
-          )
-            return true;
-          return shadowState[entity_id].state === state;
-        },
-        true,
-      );
-
-      if (isAllEntitiesCorrect) callback(event);
-      else otherwise?.(event);
-    });
-  });
-};
-
 
 type Dependency<T> = | EntityId
     | RegExp
