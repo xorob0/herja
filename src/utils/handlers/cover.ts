eb16ff0404a0d1ec8d9e0f94936345c7d75286e0
Tim <tim@xorob.io>
2022-12-06 22:25:09 +0100

    Fix cover services

--- src/utils/handlers/cover.ts
+++ src/utils/handlers/cover.ts
@@ -16,15 +16,15 @@ export const coverHandler = (entity: {entity_id:string}) =>{
     entity_id: `entity_id: "${entity.entity_id}"`,
     entity: `get entity() { return {state: shadowState["${entity.entity_id}"].state, attributes: shadowState["${entity.entity_id}"].attributes} as CoverProperties}`,
     isClosed: `isClosed() { return shadowState["${entity.entity_id}"].state === "closed" }`,
-    open: `open() { return callService("${HAEntityTypes.cover}", "open", {}, {entity_id: "${entity.entity_id}"})}`,
-    close: `close() { return callService("${HAEntityTypes.cover}", "close", {}, {entity_id: "${entity.entity_id}"})}`,
-    openTilt: `openTilt() { return callService("${HAEntityTypes.cover}", "open_tilt", {}, {entity_id: "${entity.entity_id}"})}`,
-    closeTilt: `closeTilt() { return callService("${HAEntityTypes.cover}", "close_tilt", {}, {entity_id: "${entity.entity_id}"})}`,
-    stop: `stop() { return callService("${HAEntityTypes.cover}", "stop", {}, {entity_id: "${entity.entity_id}"})}`,
-    stopTilt: `stopTilt() { return callService("${HAEntityTypes.cover}", "stop_tilt", {}, {entity_id: "${entity.entity_id}"})}`,
+    open: `open() { return callService("${HAEntityTypes.cover}", "open_cover", {}, {entity_id: "${entity.entity_id}"})}`,
+    close: `close() { return callService("${HAEntityTypes.cover}", "close_cover", {}, {entity_id: "${entity.entity_id}"})}`,
+    openTilt: `openTilt() { return callService("${HAEntityTypes.cover}", "open_cover_tilt", {}, {entity_id: "${entity.entity_id}"})}`,
+    closeTilt: `closeTilt() { return callService("${HAEntityTypes.cover}", "close_cover_tilt", {}, {entity_id: "${entity.entity_id}"})}`,
+    stop: `stop() { return callService("${HAEntityTypes.cover}", "stop_cover", {}, {entity_id: "${entity.entity_id}"})}`,
+    stopTilt: `stopTilt() { return callService("${HAEntityTypes.cover}", "stop_cover_tilt", {}, {entity_id: "${entity.entity_id}"})}`,
     toggle: `toggle() { return callService("${HAEntityTypes.cover}", "toggle", {}, {entity_id: "${entity.entity_id}"})}`,
-    setPosition: `setPosition(position: number) { return callService("${HAEntityTypes.cover}", "toggle", {position}, {entity_id: "${entity.entity_id}"})}`,
-    setTiltPosition: `setTiltPosition(tilt_position: number) { return callService("${HAEntityTypes.cover}", "toggle", {tilt_position}, {entity_id: "${entity.entity_id}"})}`,
+    setPosition: `setPosition(position: number) { return callService("${HAEntityTypes.cover}", "set_cover_position", {position}, {entity_id: "${entity.entity_id}"})}`,
+    setTiltPosition: `setTiltPosition(tilt_position: number) { return callService("${HAEntityTypes.cover}", "set_cover_tilt_position", {tilt_position}, {entity_id: "${entity.entity_id}"})}`,
   }
 }
 
